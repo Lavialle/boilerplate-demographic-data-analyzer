96df7a8e41ce121bdef07b15ef17ef5104ef0734
Ange Lavialle <ange.lavialle@student-cs.fr>
2024-10-31 16:40:28 +0000

    new commit on demographic_data_analyzer

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -12,28 +12,29 @@ def calculate_demographic_data(print_data=True):
     average_age_men = round(df.loc[df['sex'] == 'Male', 'age'].mean(),1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = round((len(df.loc[df['education'] == 'Bachelors'])/(len(df) - 1)) * 100,1)
+    percentage_bachelors = round((len(df.loc[df['education'] == 'Bachelors']) / len(df)) * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round((len(higher_education.loc[df['salary'] == '>50K']) / len(higher_education)) * 100 , 1)
+    lower_education_rich = round((len(lower_education.loc[df['salary'] == '>50K']) / len(lower_education)) * 100 , 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df.loc[df['hours-per-week'] == min_work_hours]
 
-    rich_percentage = None
+    rich_percentage = round((len(num_min_workers.loc[df['salary'] == '>50K']) / len(num_min_workers)) * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
+    print(df[df['salary' == '>50K']].groupby('native-country'))
     highest_earning_country = None
     highest_earning_country_percentage = None
 
